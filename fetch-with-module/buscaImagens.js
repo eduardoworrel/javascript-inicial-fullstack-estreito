6f189a0d8bf28903365ff60353c05702bfe25e5f
eduardoworrel <eduardo.worrel@gmail.com>
2023-06-06 21:59:32 -0300

    using classic for

--- fetch-with-module/buscaImagens.js
+++ fetch-with-module/buscaImagens.js
@@ -3,7 +3,8 @@ export function buscaImagens(elemento){
     fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY")
     .then(function(e){return e.json()})
     .then(data => {
-        for(let foto of data.photos){
+        for(let i = 0; i < data.photos.length; i++){
+            let foto = data.photos[i]
             const url = foto.img_src;
 
             const img = document.createElement("img");
